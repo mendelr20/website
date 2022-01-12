b8b0ec0ef7f5a43bf14807978c2dcabf004efa77
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-01-12 11:10:52 -0500

    0.1

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Mendel Rosenblum - NREMRT, NJ EMT, NY EMT, Paramedic student, Software Engineering Student  ";
 document.querySelector("body").appendChild(h2);
